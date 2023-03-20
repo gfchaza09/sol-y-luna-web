9f18e9283d827178b805c2a68b51e6488d2a15b0
Gabriel Chazarreta <gfchazarreta@gmail.com>
2023-03-20 14:07:19 -0300

    Adding HomeLocationsMobile

--- src/components/HomeLocations/HomeLocations.jsx
+++ src/components/HomeLocations/HomeLocations.jsx
@@ -36,6 +36,7 @@ export const HomeLocationsDesktop = ({ selectedTheme }) => {
 
   return (
     <Container
+      component="section"
       sx={{
         padding: "0px",
         display: "flex",
@@ -167,7 +168,7 @@ export const HomeLocationsDesktop = ({ selectedTheme }) => {
             </Container>
             <Typography
               variant="h3"
-              onClick={() => setLocalActive("alcanfores")}
+              onClick={() => setLocalActive("centro")}
               sx={{ cursor: "pointer" }}
             >
               CENTRO
@@ -214,7 +215,7 @@ export const HomeLocationsDesktop = ({ selectedTheme }) => {
             </Container>
             <Typography
               variant="h3"
-              onClick={() => setLocalActive("alcanfores")}
+              onClick={() => setLocalActive("boulevard")}
               sx={{ cursor: "pointer" }}
             >
               BOULEVARD
@@ -245,3 +246,228 @@ export const HomeLocationsDesktop = ({ selectedTheme }) => {
   );
 };
 
+export const HomeLocationsMobile = ({selectedTheme}) => {
+  const [localActive, setLocalActive] = useState("alcanfores");
+
+  return (
+    <Container
+      component="section"
+      sx={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "50px",
+      }}
+    >
+      <Container
+        sx={{ padding: "0px", marginBottom: "50px" }}
+      >
+        <Typography component="h2" variant="h5" sx={{ marginBottom: "24px", textAlign: "center" }}>
+          UBICACIONES
+        </Typography>
+        <Typography>
+          Contamos con 3 sucursales distribuidas por la ciudad
+        </Typography>
+        <Typography>
+          En todas ellas podrás encontrar un agradable ambiente familiar y
+          disfrutar de nuestra gran variedad de platillos.
+        </Typography>
+      </Container>
+      <Container sx={{ padding: "0px", height: "auto", marginBottom: "50px" }}>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            position: "relative",
+            gap: "30px"
+          }}
+        >
+          <Container
+            sx={{
+              padding: "0px",
+              position: "absolute",
+              top: "26px",
+              left: {mobile: "29px", tablet: "43px"},
+              height: {mobile: `${localActive === "boulevard" ? "27%" :"90%"}`, mobile2: `${localActive === "boulevard" ? "24%" :"90%"}`, tablet: `${localActive === "boulevard" ? "30%" :"90%"}`},
+              width: "2px",
+              border: "2px solid",
+              borderColor: "primary.border",
+            }}
+          ></Container>
+          <Container
+            sx={{
+              padding: "0px",
+              display: "flex",
+              alignItems: "center",
+              gap: "25px",
+            }}
+          >
+            <Container
+              sx={{
+                padding: { mobile: "4px", tablet: "8px" },
+                margin: "0px",
+                width: { mobile: "30px", tablet: "58px" },
+                height: { mobile: "30px", tablet: "58px" },
+                border: "3px solid",
+                borderColor: `${
+                  localActive === "alcanfores"
+                    ? "primary.borderActive"
+                    : "primary.border"
+                }`,
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                opacity: `${localActive === "alcanfores" ? "1" : "0.9"}`,
+              }}
+              onClick={() => setLocalActive("alcanfores")}
+            >
+              <ButtonBase
+                variant="menuButton"
+                sx={{
+                  borderRadius: "50%",
+                  width: "100%",
+                  height: "100%",
+                  background: "",
+                }}
+              ></ButtonBase>
+            </Container>
+            <Typography
+              variant="h5"
+              onClick={() => setLocalActive("alcanfores")}
+              sx={{ cursor: "pointer" }}
+            >
+              ALCANFORES
+            </Typography>
+            
+          </Container>
+          <Container sx={{paddingLeft: "30px", paddingRight: "0px"}}>
+            {
+                localActive === "alcanfores" && <LocalCard
+                data={locations[0]}
+                selectedTheme={selectedTheme}
+                localActive={localActive}
+              />
+              }
+
+          </Container>
+          <Container
+            sx={{
+              padding: "0px",
+              display: "flex",
+              alignItems: "center",
+              gap: "25px",
+            }}
+          >
+            <Container
+              sx={{
+                padding: { mobile: "4px", tablet: "8px" },
+                margin: "0px",
+                width: { mobile: "30px", tablet: "58px" },
+                height: { mobile: "30px", tablet: "58px" },
+                border: "3px solid",
+                borderColor: `${
+                  localActive === "centro"
+                    ? "primary.borderActive"
+                    : "primary.border"
+                }`,
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                opacity: `${localActive === "centro" ? "1" : "0.9"}`,
+              }}
+              onClick={() => setLocalActive("centro")}
+            >
+              <ButtonBase
+                variant="menuButton"
+                sx={{
+                  borderRadius: "50%",
+                  width: "100%",
+                  height: "100%",
+                  background: "",
+                }}
+              ></ButtonBase>
+            </Container>
+            <Typography
+              variant="h5"
+              onClick={() => setLocalActive("centro")}
+              sx={{ cursor: "pointer" }}
+            >
+              CENTRO
+            </Typography>
+          </Container>
+          <Container sx={{paddingLeft: "30px", paddingRight: "0px"}}>
+            {
+                localActive === "centro" && <LocalCard
+                data={locations[1]}
+                selectedTheme={selectedTheme}
+                localActive={localActive}
+              />
+              }
+          </Container>
+          <Container
+            sx={{
+              padding: "0px",
+              display: "flex",
+              alignItems: "center",
+              gap: "25px",
+            }}
+          >
+            <Container
+              sx={{
+                padding: { mobile: "4px", tablet: "8px" },
+                margin: "0px",
+                width: { mobile: "30px", tablet: "58px" },
+                height: { mobile: "30px", tablet: "58px" },
+                border: "3px solid",
+                borderColor: `${
+                  localActive === "boulevard"
+                    ? "primary.borderActive"
+                    : "primary.border"
+                }`,
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                opacity: `${localActive === "boulevard" ? "1" : "0.9"}`,
+              }}
+              onClick={() => setLocalActive("boulevard")}
+            >
+              <ButtonBase
+                variant="menuButton"
+                sx={{
+                  borderRadius: "50%",
+                  width: "100%",
+                  height: "100%",
+                  background: "",
+                }}
+              ></ButtonBase>
+            </Container>
+            <Typography
+              variant="h5"
+              onClick={() => setLocalActive("boulevard")}
+              sx={{ cursor: "pointer" }}
+            >
+              BOULEVARD
+            </Typography>
+          </Container>
+          <Container sx={{paddingLeft: "30px", paddingRight: "0px"}}>
+            {
+                localActive === "boulevard" && <LocalCard
+                data={locations[2]}
+                selectedTheme={selectedTheme}
+                localActive={localActive}
+              />
+            }
+          </Container>
+        </Container>
+      </Container>
+      <ButtonComponent href="/ubicaciones">Ver ubicaciones</ButtonComponent>
+    </Container>
+  );
+}
