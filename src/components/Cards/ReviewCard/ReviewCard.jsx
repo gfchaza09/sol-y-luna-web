2e0c10f38dd9e25feb54f665d127b932b2e5e1f6
Gabriel Chazarreta <gfchazarreta@gmail.com>
2023-03-21 23:51:27 -0300

    Updating ReviewCard component

--- src/components/Cards/ReviewCard/ReviewCard.jsx
+++ src/components/Cards/ReviewCard/ReviewCard.jsx
@@ -5,7 +5,7 @@ import { Card, Typography } from '@mui/material';
 import styles from './ReviewCard.module.css';
 import { useWindowWidth } from '@/hooks/useWindowWidth';
 
-const {card__container, review__container, data__container, personaldata__container, date__container, rating__container, rating} = styles;
+const {card__container, review__container, data__container, personaldata__container, date__container, social__network, rating__container, rating} = styles;
 
 const ReviewCard = () => {
 
@@ -16,17 +16,20 @@ const ReviewCard = () => {
       <div className={review__container}>
         <Image src="https://upload.wikimedia.org/wikipedia/commons/9/90/El_empleado_del_mes.jpg" width={72} height={72} alt="review"/>
         <div className={data__container}>
-          <Typography className={width < 375 ? 'review-text-mobile' : 'review-text'}>El lugar agradable, buena música y la atención muy amable. La comida está rica también y bien servida, también el precio está accesible. Variedad! En familia o pareja está bonito.</Typography>
+          <Typography variant={width < 640 ? 'body2' : 'body1'}>El lugar agradable, buena música y la atención muy amable. La comida está rica también y bien servida, también el precio está accesible. Variedad! En familia o pareja está bonito.</Typography>
           <div className={personaldata__container}>
             <div className={date__container}>
               <Typography className='button'>Loki Bright</Typography>
               <Typography className='card-date'>31 de julio de 2022</Typography>
             </div>
-            <div className={rating__container}>
-              <Typography className={'button '+rating}>4.5 <StarIcon color='#F5C537' width={22} height={22}/></Typography>
-            </div>
           </div>
-        </div> 
+        </div>
+        <div className={social__network}>
+          <Image src="/assets/social-networks/tripadvisor-review.png" alt="social-network" width={28} height={28}/>
+        </div>
+        <div className={rating__container}>
+          <Typography className={'button '+rating}>4.5 <StarIcon color='#F5C537' width={22} height={22}/></Typography>
+        </div>
       </div>
     </Card>
   )
