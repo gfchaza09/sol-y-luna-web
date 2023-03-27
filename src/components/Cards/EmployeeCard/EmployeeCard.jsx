297a9cc83392b55f3f1d17f8b3a600f86f0ab26a
Gabriel Chazarreta <gfchazarreta@gmail.com>
2023-03-27 20:34:57 -0300

    Fix EmployeeCard

--- src/components/Cards/EmployeeCard/EmployeeCard.jsx
+++ src/components/Cards/EmployeeCard/EmployeeCard.jsx
@@ -12,12 +12,12 @@ const EmployeeCard = ({data}) => {
 
   return (
     <Card variant="cardBackground" className={card__container} sx={{borderRadius: "12px"}}>
-      <Container sx={{padding: "0px", position: "relative", height: "280px", overflow: "hidden", borderRadius: "6px"}}>
+      <Container sx={{padding: "0px", position: "relative", height: "270px", overflow: "hidden", borderRadius: "6px"}}>
         <Image src={data.img} fill sizes="100vh" alt="employee" style={{ width: "100%", height: "100%",objectFit: "cover"}}/>
       </Container>
-      <Typography component="h3" variant='h3' className="h3" sx={{marginTop: "24px"}}>{data.nombre}</Typography>
+      <Typography component="h3" variant='h3' className="h3" sx={{marginTop: "16px"}}>{data.nombre}</Typography>
       <Typography component="h4" variant='subtitleCard' className='h4' sx={{marginTop: "8px", textAlign: "center"}}>{data.cargo} - {data.sucursal}</Typography>
-      <Typography sx={{marginTop: "8px", fontWeight: {moible: 400, tablet: 500}, fontSize: {mobile: 14, tablet: 16}, lineHeight: {mobile: "20px",tablet: "25px"}}}>{data.descripcion}</Typography>
+      <Typography sx={{marginTop: "8px", fontWeight: {moible: 400, tablet: 500}, fontSize: {mobile: 14, tablet: 16}, lineHeight: {mobile: "20px",tablet: "22px"}}}>{data.descripcion}</Typography>
     </Card>
   )
 }
